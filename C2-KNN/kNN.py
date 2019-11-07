650186bda88cea688ce6619fdb1921aaf85aeafd
Mingy2018 <605329720@qq.com>
2019-11-08 00:50:40 +0100

    finish 2-1

--- C2-KNN/kNN.py
+++ C2-KNN/kNN.py
@@ -9,16 +9,16 @@ def createDataSet():
 def classify0(inX, dataSet, labels, k):
     #-----------Calculation of the distance----------------------
     dataSetSize=dataSet.shape[0] #number of samples in TraingSet
-    diffMat=tile(inx, (dataSetSize,1))-dataSet # difference vector of inX with each samples
+    diffMat=tile(inX, (dataSetSize,1))-dataSet # difference vector of inX with each samples
     sqDiffMat=diffMat**2 # square based on each elements
-    sqDistances=sqDiffMat.sum(aixs=1)
+    sqDistances=sqDiffMat.sum(axis=1)
     distances=sqDistances**0.5
     #------------sort-----------------------
-    sortedDistIndicies=distances.argsort()
+    sortedDistIndicies=distances.argsort()# indices!! from small to big
     classCount={}
     for i in range(k):
         voteIlabel=labels[sortedDistIndicies[i]]
-        classCount[voteIlabel]=classCount.get(voteIlabel,0)+1
-    sortedClassCount=sorted(classCount.iteritems(),
+        classCount[voteIlabel]=classCount.get(voteIlabel,0)+1 # count the k nearst elements based on labels
+    sortedClassCount=sorted(classCount.items(),
                             key=operator.itemgetter(1),reverse=True)
-    return  sortedClassCount[0][0]
+    return sortedClassCount[0][0]
