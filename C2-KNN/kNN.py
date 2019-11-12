b1636638408b69d0554c91dd2d6e170e6ad5e839
Mingy2018 <605329720@qq.com>
2019-11-12 14:21:28 +0100

    test

--- C2-KNN/kNN.py
+++ C2-KNN/kNN.py
@@ -58,6 +58,10 @@ def datingClassTest():
     for i in range(numTestVecs):
         classifierResult=classify0(normMat[i,:], normMat[numTestVecs:m,:],
                                    datingLabels[numTestVecs:m,3])
+        print("the classifier came back with: %d, the real answer is: %d"\
+              %(classifierResult,datingLabels[i]))
+        if (classifierResult!=datingLabels[i]): errorCount+=1.0
+    print("the total error rate is: %f" % (errorCount/float(numTestVecs))
 
 
 
